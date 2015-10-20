faba67b529c93be22ba4077a600497bcfc47ac27
Andras Pahi <Derwish@Andras-Pahis-MacBook-Air.local>
2015-10-20 19:12:54 +0200

    _w,_p init moved to kinit()

--- src/kc.c
+++ src/kc.c
@@ -62,7 +62,7 @@ I args(int n,S*v) {
         kK(KONA_ARGS)[i]=a )
   while(-1!=(c=getopt(n,v,":h:i:e:x:")))SW(c) {
     CS('h',  if(IPC_PORT)O("-i accepted, cannot also have -h\n"); else HTTP_PORT=optarg;)
-    CS('i',  if(HTTP_PORT)O("-h accepted, cannot also have -i\n"); else IPC_PORT=optarg;)
+    CS('i',  if(HTTP_PORT)O("-h accepted, cannot also have -i\n"); else {IPC_PORT=optarg;*kI(KONA_PORT)=atol(IPC_PORT);})
     CS('e',  cd(X(optarg)); exit(0) )
     CS('x',  k=X(optarg); printAtDepth(0,k,0,0,0,0); O("\n"); cd(k); exit(0) )
     CSR(':', )
@@ -119,6 +119,8 @@ I kinit() {       //oom (return bad)
   kap(&KTREE,&x); cd(x);
   x=newE(sp("t"),_dot_t());
   kap(&KTREE,&x); cd(x);
+  KONA_WHO=newK(1,1);*kI(KONA_WHO)=0;
+  KONA_PORT=newK(1,1);*kI(KONA_PORT)=0;
   R 0; }
 
 Z I randomBits(){
@@ -275,10 +277,6 @@ I attend() {  //K3.2 uses fcntl somewhere
   I yes=1;	// for setsockopt() SO_REUSEADDR, below 
   int i, rv;
   struct addrinfo hints, *ai, *p;
-
-  KONA_WHO=newK(1,1);*kI(KONA_WHO)=0;
-  KONA_PORT=newK(1,1);*kI(KONA_PORT)=IPC_PORT?atoi(IPC_PORT):0;
-
   FD_ZERO(&master);	// clear the master and temp sets
   FD_ZERO(&read_fds);
 
@@ -456,9 +454,6 @@ void *socket_thread(void *arg) {
   hints.ai_protocol = IPPROTO_TCP;
   hints.ai_flags =    AI_PASSIVE;
 
-  KONA_WHO=newK(1,1);*kI(KONA_WHO)=0;
-  KONA_PORT=newK(1,1);*kI(KONA_PORT)=IPC_PORT?atoi(IPC_PORT):0;
-  
   // resolve local address and port
   if(IPC_PORT){if((rv=getaddrinfo(NULL, IPC_PORT, &hints, &result)) != 0){O("server: %s\n", gai_strerror(rv)); exit(1);}}
   if(HTTP_PORT){if((rv=getaddrinfo(NULL, HTTP_PORT, &hints, &result)) != 0){O("server: %s\n", gai_strerror(rv)); exit(1);}}
